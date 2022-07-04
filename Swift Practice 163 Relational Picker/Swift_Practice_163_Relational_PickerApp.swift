a868ea96683520cd52ee9108139ac5189ee539d1
Dogpa <dogpa0130@gmail.com>
2022-07-04 21:28:51 +0800

    Initial Commit

---
+++ Swift Practice 163 Relational Picker/Swift_Practice_163_Relational_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_163_Relational_PickerApp.swift
+//  Swift Practice 163 Relational Picker
+//
+//  Created by Dogpa's MBAir M1 on 2022/7/4.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_163_Relational_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
